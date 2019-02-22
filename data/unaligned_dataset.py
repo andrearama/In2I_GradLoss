1b2d55348220868278665d255c7d18bd77c890c6
Andrea Ramazzina <aramazzi@blwd.dhcp.nordic.volvocars.net>
2019-02-22 12:01:30 +0100

    change input reading from vertical to horizontal

--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -27,7 +27,7 @@ class UnalignedDataset(BaseDataset):
         osize = [opt.loadSize, opt.loadSize*self.opt.input_nc]
         opt.fineSize*self.no_input*self.opt.input_nc
         self.transformA = []
-        self.transformA.append(transforms.Scale((opt.fineSize*2 , opt.fineSize), Image.BICUBIC))
+        self.transformA.append(transforms.Scale((opt.fineSize , opt.fineSize*2), Image.BICUBIC))
         self.transformA += [transforms.ToTensor(),
                        transforms.Normalize((0.5, 0.5, 0.5),
                                             (0.5, 0.5, 0.5))]
@@ -42,10 +42,11 @@ class UnalignedDataset(BaseDataset):
         # print('(A, B) = (%d, %d)' % (index_A, index_B))
         A_img = Image.open(A_path)#.convert('RGB') # A image is a no_input*3 collection of images
         A_img = (self.transformA(A_img))
-        A1 = A_img[1,0:256,:]
-        A2 = A_img[1,256:512,:]
-        A1 = A1.unsqueeze(0).numpy()
-        A2 = A2.unsqueeze(0).numpy()
+        A1 = A_img[:,:,0:256]
+        A2 = A_img[:,:,256:512]
+#        A1 = A1.unsqueeze(0).numpy()
+#        A2 = A2.unsqueeze(0).numpy()
+#        A1 = self.transform(A1)
         B_img = Image.open(B_path)#.convert('RGB')
         B = self.transform(B_img)
         if self.opt.which_direction == 'BtoA':
